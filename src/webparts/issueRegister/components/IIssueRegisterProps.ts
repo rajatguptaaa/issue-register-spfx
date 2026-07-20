1a6aaf72bb08cf0042b55897774d43d6f9022ca0
Rajat Kumar Gupta <107606853+rajatguptaaa@users.noreply.github.com>
2026-07-21 00:53:32 +0530

    Initial commit: SPFx project scaffold (Issue Register)

---
+++ src/webparts/issueRegister/components/IIssueRegisterProps.ts
@@ -0,0 +1,6 @@
+export interface IIssueRegisterProps {
+  description: string;
+  isDarkTheme: boolean;
+  environmentMessage: string;
+  userDisplayName: string;
+}
---
+++ src/webparts/issueRegister/loc/mystrings.d.ts
@@ -0,0 +1,19 @@
+declare interface IIssueRegisterWebPartStrings {
+  PropertyPaneDescription: string;
+  BasicGroupName: string;
+  DescriptionFieldLabel: string;
+  AppLocalEnvironmentSharePoint: string;
+  AppLocalEnvironmentTeams: string;
+  AppLocalEnvironmentOffice: string;
+  AppLocalEnvironmentOutlook: string;
+  AppSharePointEnvironment: string;
+  AppTeamsTabEnvironment: string;
+  AppOfficeEnvironment: string;
+  AppOutlookEnvironment: string;
+  UnknownEnvironment: string;
+}
+
+declare module 'IssueRegisterWebPartStrings' {
+  const strings: IIssueRegisterWebPartStrings;
+  export = strings;
+}
